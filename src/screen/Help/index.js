a8cbd009444330d0ac9a456c3f36aa7c306b28ff
worker722 <tech.worker722@gmail.com>
2020-11-17 18:09:05 +0100

    fdsaga

--- src/screen/Help/index.js
+++ src/screen/Help/index.js
@@ -22,11 +22,11 @@ export default class Help extends Component {
         const navigation = this.props.navigation;
         return (
             <View style={{ flex: 1, paddingHorizontal: 10 }}>
-                <Header icon_left={"arrow-left"} title={"Help"} callback_left={this.closeCallback} />
+                <Header icon_left={"arrow-left"} title={"Help & Support"} callback_left={this.closeCallback} />
                 <TouchableOpacity style={{ width: "100%", marginTop: 50, flexDirection: "row", paddingHorizontal: 20 }}>
                     <View style={{ justifyContent: "center", flex: 1 }}>
-                        <Text style={{ fontSize: 18 }}>Invoices</Text>
-                        <Text style={{ color: BaseColor.grayColor }}>See and download you invoices</Text>
+                        <Text style={{ fontSize: 18 }}>Help Center</Text>
+                        <Text style={{ color: BaseColor.grayColor }}>See FAQ and contact support</Text>
                     </View>
                     <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center", marginRight: 10 }}>
                         <Icon name={"angle-right"} size={25} color={BaseColor.grayColor}></Icon>
@@ -35,8 +35,28 @@ export default class Help extends Component {
                 <View style={{ backgroundColor: "#ddd", width: "98%", height: 1, marginTop: 10, marginHorizontal: "1%" }}></View>
                 <TouchableOpacity style={{ width: "100%", marginTop: 20, flexDirection: "row", paddingHorizontal: 20 }}>
                     <View style={{ justifyContent: "center", flex: 1 }}>
-                        <Text style={{ fontSize: 18 }}>Billing infomation</Text>
-                        <Text style={{ color: BaseColor.grayColor }}>Edit your billing name and address, etc</Text>
+                        <Text style={{ fontSize: 18 }}>Rate us</Text>
+                        <Text style={{ color: BaseColor.grayColor }}>if you love our app, please rake a moment to rate it</Text>
+                    </View>
+                    <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center", marginRight: 10 }}>
+                        <Icon name={"angle-right"} size={25} color={BaseColor.grayColor}></Icon>
+                    </View>
+                </TouchableOpacity>
+                <View style={{ backgroundColor: "#ddd", width: "98%", height: 1, marginTop: 10, marginHorizontal: "1%" }}></View>
+                <TouchableOpacity style={{ width: "100%", marginTop: 20, flexDirection: "row", paddingHorizontal: 20 }}>
+                    <View style={{ justifyContent: "center", flex: 1 }}>
+                        <Text style={{ fontSize: 18 }}>Invite friends Fetch</Text>
+                        <Text style={{ color: BaseColor.grayColor }}>Invite your friend to buy and sell pets</Text>
+                    </View>
+                    <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center", marginRight: 10 }}>
+                        <Icon name={"angle-right"} size={25} color={BaseColor.grayColor}></Icon>
+                    </View>
+                </TouchableOpacity>
+                <View style={{ backgroundColor: "#ddd", width: "98%", height: 1, marginTop: 10, marginHorizontal: "1%" }}></View>
+                <TouchableOpacity style={{ width: "100%", marginTop: 20, flexDirection: "row", paddingHorizontal: 20 }}>
+                    <View style={{ justifyContent: "center", flex: 1 }}>
+                        <Text style={{ fontSize: 18 }}>Version</Text>
+                        <Text style={{ color: BaseColor.grayColor }}>14.13.002</Text>
                     </View>
                     <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center", marginRight: 10 }}>
                         <Icon name={"angle-right"} size={25} color={BaseColor.grayColor}></Icon>
