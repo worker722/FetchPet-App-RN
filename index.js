fac8e3935525b97181e2ba9fb11b7c10738fcdd9
sunny <sunny.king1219@gmail.com>
2020-11-22 12:26:29 +0100

    @firebase messaging

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 
+import React from 'react';
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './src/app.json';
+import messaging from '@react-native-firebase/messaging';
 
-AppRegistry.registerComponent(appName, () => App);
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+    console.log('Message handled in the background!', remoteMessage);
+});
+
+function HeadlessCheck({ isHeadless }) {
+    if (isHeadless) {
+        // App has been launched in the background by iOS, ignore
+        return null;
+    }
+
+    return <App />;
+}
+
+AppRegistry.registerComponent(appName, () => HeadlessCheck);
