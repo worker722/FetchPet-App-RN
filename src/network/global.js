4058d1cf56efad86a81753efad78e61e7d0b5a47
suunyking1219 <sunny.king1219@gmail.com>
2020-12-13 23:32:56 -0800

    ddsds

--- src/network/global.js
+++ src/network/global.js
@@ -17,5 +17,7 @@ export const _SHOW_PHONE_ON_ADS = '_show_phone_on_ads';
 //ANDROID PACKAGE, APPLEAPP ID
 export const ANDROID_PACKAGE = 'com.darryl.fetch';
 export const APPLE_APP_ID = '1544704957';
+export const APPLE_APP_LINK = 'https://apps.apple.com/us/app/id1544704957';
+export const GOOGLE_APP_LINK = 'https://apps.apple.com/us/app/id1544704957';
 
 
