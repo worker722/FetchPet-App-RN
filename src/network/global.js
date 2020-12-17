cda84b9f88b607cfe8a6c553bbafdb40d8d66fda
sunny <sunny.king1219@gmail.com>
2020-12-17 19:44:17 +0100

    sasdsds

--- src/network/global.js
+++ src/network/global.js
@@ -15,7 +15,7 @@ export const _SHOW_NOTIFICATION = '_show_notification';
 export const _SHOW_PHONE_ON_ADS = '_show_phone_on_ads';
 
 //ANDROID PACKAGE, APPLEAPP ID
-export const ANDROID_PACKAGE = 'com.darryl.fetch';
+export const ANDROID_PACKAGE = 'fetch.market';
 export const APPLE_APP_ID = '1544704957';
 export const APPLE_APP_LINK = `https://apps.apple.com/us/app/id${APPLE_APP_ID}`;
 export const GOOGLE_APP_LINK = `https://play.google.com/store/apps/details?id=${ANDROID_PACKAGE}`;
