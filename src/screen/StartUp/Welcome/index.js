bb3ae8a2a6fe8494c9320a5c74159d1b60b9cb0a
sunny <sunny.king1219@gmail.com>
2020-12-18 03:20:00 +0100

    dssddsds

--- src/screen/StartUp/Welcome/index.js
+++ src/screen/StartUp/Welcome/index.js
@@ -3,8 +3,6 @@ import {
     View,
     TouchableOpacity,
     Text,
-    Platform,
-    PermissionsAndroid
 } from 'react-native';
 
 import { Image } from 'react-native-elements';
@@ -17,38 +15,6 @@ export default class Welcome extends Component {
     constructor(props) {
         super(props);
     }
-
-    componentDidMount = () => {
-        this.requestPermission();
-    }
-
-    requestPermission = async () => {
-        try {
-            if (Platform.OS != "android")
-                return;
-
-            const granted = await PermissionsAndroid.requestMultiple(
-                [
-                    PermissionsAndroid.PERMISSIONS.CAMERA,
-                    PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
-                    PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
-                    PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-                    PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION
-                ],
-            );
-            if (
-                granted['android.permission.CAMERA'] === PermissionsAndroid.RESULTS.GRANTED &&
-                granted['android.permission.READ_EXTERNAL_STORAGE'] === PermissionsAndroid.RESULTS.GRANTED &&
-                granted['android.permission.WRITE_EXTERNAL_STORAGE'] === PermissionsAndroid.RESULTS.GRANTED &&
-                granted['android.permission.ACCESS_FINE_LOCATION'] === PermissionsAndroid.RESULTS.GRANTED &&
-                granted['android.permission.ACCESS_COARSE_LOCATION'] === PermissionsAndroid.RESULTS.GRANTED
-            ) {
-                console.log('permission ok');
-            }
-
-        } catch (err) {
-        }
-    };
 
     render = () => {
         return (
