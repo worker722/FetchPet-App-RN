56cc853e7030ed5238fc842bacad0f7486c71689
worker722 <tech.worker722@gmail.com>
2020-12-19 13:17:24 +0100

    dsdsdsdsdssdsds

--- src/config/images.js
+++ src/config/images.js
@@ -1,7 +1,7 @@
 
 export const Images = {
-  welcome: require("@assets/images/welcome.jpg"),
-  sign: require("@assets/images/sign.jpg"),
+  welcome: require("@assets/images/welcome.png"),
+  sign: require("@assets/images/sign.png"),
   logo: require("@assets/images/logo.png"),
   ic_sell: require("@assets/images/sell.png"),
   ic_sell_fill: require("@assets/images/sell-fill.png"),
