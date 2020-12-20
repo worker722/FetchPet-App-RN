153f69ef170bf2b59461140eb49711136014653d
worker722 <tech.worker722@gmail.com>
2020-12-20 15:17:48 +0100

    ]['

--- src/screen/MyAds/index.js
+++ src/screen/MyAds/index.js
@@ -14,13 +14,21 @@ export default class MyAds extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            index: 0
+            currentTabIndex: 0
         }
+
+        props.navigation.addListener("willFocus", (event) => {
+            this.UNSAFE_componentWillMount();
+        });
+    }
+
+    UNSAFE_componentWillMount = () => {
+        this.setState({ currentTabIndex: 0 });
     }
 
     render = () => {
         const navigation = this.props.navigation;
-        const { index } = this.state;
+        const { currentTabIndex } = this.state;
 
         return (
             <View style={{ flex: 1 }}>
@@ -28,24 +36,24 @@ export default class MyAds extends Component {
                 <Text style={{ color: BaseColor.primaryColor, fontSize: 20, fontWeight: "bold", paddingLeft: 10 }}>My Ads</Text>
                 <View style={{ flex: 1, marginTop: 10 }}>
                     <View style={{ flexDirection: "row" }}>
-                        <TouchableOpacity style={{ flex: 1, justifyContent: "center", alignItems: "center", padding: 10, }} onPress={() => this.setState({ index: 0 })}>
-                            <Text style={{ color: index == 0 ? BaseColor.primaryColor : BaseColor.greyColor }}>Favourite</Text>
+                        <TouchableOpacity style={{ flex: 1, justifyContent: "center", alignItems: "center", padding: 10, }} onPress={() => this.setState({ currentTabIndex: 0 })}>
+                            <Text style={{ color: currentTabIndex == 0 ? BaseColor.primaryColor : BaseColor.greyColor }}>Favourite</Text>
                         </TouchableOpacity>
-                        <TouchableOpacity style={{ flex: 1, justifyContent: "center", alignItems: "center", padding: 10, }} onPress={() => this.setState({ index: 1 })}>
-                            <Text style={{ color: index == 1 ? BaseColor.primaryColor : BaseColor.greyColor }}>Active</Text>
+                        <TouchableOpacity style={{ flex: 1, justifyContent: "center", alignItems: "center", padding: 10, }} onPress={() => this.setState({ currentTabIndex: 1 })}>
+                            <Text style={{ color: currentTabIndex == 1 ? BaseColor.primaryColor : BaseColor.greyColor }}>Active</Text>
                         </TouchableOpacity>
-                        <TouchableOpacity style={{ flex: 1, justifyContent: "center", alignItems: "center", padding: 10 }} onPress={() => this.setState({ index: 2 })}>
-                            <Text style={{ color: index == 2 ? BaseColor.primaryColor : BaseColor.greyColor }}>Closed</Text>
+                        <TouchableOpacity style={{ flex: 1, justifyContent: "center", alignItems: "center", padding: 10 }} onPress={() => this.setState({ currentTabIndex: 2 })}>
+                            <Text style={{ color: currentTabIndex == 2 ? BaseColor.primaryColor : BaseColor.greyColor }}>Closed</Text>
                         </TouchableOpacity>
                     </View>
                     <View style={{ flexDirection: "row" }}>
-                        <View style={{ flex: 1, height: 5, backgroundColor: index == 0 ? BaseColor.primaryColor : BaseColor.whiteColor }}></View>
-                        <View style={{ flex: 1, height: 5, backgroundColor: index == 1 ? BaseColor.primaryColor : BaseColor.whiteColor }}></View>
-                        <View style={{ flex: 1, height: 5, backgroundColor: index == 2 ? BaseColor.primaryColor : BaseColor.whiteColor }}></View>
+                        <View style={{ flex: 1, height: 5, backgroundColor: currentTabIndex == 0 ? BaseColor.primaryColor : BaseColor.whiteColor }}></View>
+                        <View style={{ flex: 1, height: 5, backgroundColor: currentTabIndex == 1 ? BaseColor.primaryColor : BaseColor.whiteColor }}></View>
+                        <View style={{ flex: 1, height: 5, backgroundColor: currentTabIndex == 2 ? BaseColor.primaryColor : BaseColor.whiteColor }}></View>
                     </View>
-                    {this.state.index == 0 && <Favourite navigation={this.props.navigation} />}
-                    {this.state.index == 1 && <Active navigation={this.props.navigation} />}
-                    {this.state.index == 2 && <Closed navigation={this.props.navigation} />}
+                    {currentTabIndex == 0 && <Favourite navigation={this.props.navigation} />}
+                    {currentTabIndex == 1 && <Active navigation={this.props.navigation} />}
+                    {currentTabIndex == 2 && <Closed navigation={this.props.navigation} />}
                 </View>
             </View>
         )
