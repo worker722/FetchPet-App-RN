bae346a61a73fd4e592472f9581a44ecfa862389
sunny <sunny.king1219@gmail.com>
2020-12-21 20:21:38 +0100

    sadgfsa

--- src/screen/Notification/index.js
+++ src/screen/Notification/index.js
@@ -103,7 +103,7 @@ class Notification extends Component {
             return (<Loader />);
 
         return (
-            <View style={{ flex: 1 }}>
+            <View style={{ flex: 1, marginBottom: 10 }}>
                 <Header title={"Notification"} icon_left={"times-circle"} callback_left={this.goBack} />
                 <Text style={{ fontSize: 18, color: BaseColor.primaryColor, paddingHorizontal: 20 }}>Notification</Text>
                 <ScrollView
