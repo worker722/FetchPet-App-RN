9981c2fb300511833182a5192e0ba898a0e98a9c
sunny <sunny.king1219@gmail.com>
2020-12-30 21:58:25 +0100

    change package name

--- src/network/global.js
+++ src/network/global.js
@@ -24,7 +24,7 @@ export const PREF_APPLE_NAME = 'PREF_APPLE_NAME';
 export const PREF_SHOW_APPLE_BUTTON = 'PREF_SHOW_APPLE_BUTTON';
 
 //ANDROID PACKAGE, APPLEAPP ID
-export const ANDROID_PACKAGE = 'fetch.market';
+export const ANDROID_PACKAGE = 'com.darryl.fetch';
 export const APPLE_APP_ID = '1545467871';
 export const APPLE_APP_LINK = `https://apps.apple.com/us/app/id${APPLE_APP_ID}`;
 export const GOOGLE_APP_LINK = `https://play.google.com/store/apps/details?id=${ANDROID_PACKAGE}`;
