8ad0e631f67b374b0b5733c254de7ed7945309c2
worker722 <tech.worker722@gmail.com>
2021-01-12 00:52:23 +0100

    3212121

--- src/store/index.js
+++ src/store/index.js
@@ -9,14 +9,14 @@ import rootReducer from "@reducers";
  * Redux Setting
  */
 const persistConfig = {
-  key: "root",
-  storage: AsyncStorage,
-  timeout: 100000
+	key: "root",
+	storage: AsyncStorage,
+	timeout: 100000
 };
 
 let middleware = [thunk];
 if (process.env.NODE_ENV === `development`) {
-  middleware.push(logger);
+	middleware.push(logger);
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
@@ -24,18 +24,18 @@ const store = createStore(persistedReducer, applyMiddleware(...middleware));
 const persistor = persistStore(store);
 
 const SetPrefrence = async (key, value) => {
-  try {
-    await AsyncStorage.setItem(key, String(value));
-  } catch (error) {
-  }
+	try {
+		await AsyncStorage.setItem(key, String(value));
+	} catch (error) {
+	}
 }
 
 const GetPrefrence = async (key) => {
-  try {
-    const value = await AsyncStorage.getItem(key)
-    return value;
-  } catch (error) {
-    return null;
-  }
+	try {
+		const value = await AsyncStorage.getItem(key)
+		return value;
+	} catch (error) {
+		return null;
+	}
 }
 export { store, persistor, SetPrefrence, GetPrefrence };
