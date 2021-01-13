0d5d4770e3c76e7a8e2611075b3b44d570abbc50
sunny <sunny.king1219@gmail.com>
2021-01-13 17:33:06 +0100

    dsdsds

--- src/bgMessaging/index.js
+++ src/bgMessaging/index.js
@@ -1,17 +1,22 @@
 import * as global from "@api/global";
+import { SetPrefrence } from "@store";
 
-export default (notification) => {
-  console.log('remote notification when background');
-  try {
-    const { data } = notification;
-    const notificationData = JSON.parse(data.data);
-    if (notificationData.notification_type == global.CHAT_MESSAGE_NOTIFICATION) {
-      if (!this.props.IS_IN_CHAT)
-        this.props.setStore(global.U_MESSAGE_INCREMENT, 1);
-    }
-  } catch (error) {
-    console.log('remote notification received error', error);
-  }
+export default async (notification) => {
+	try {
+		const { data } = notification;
+		const notificationData = JSON.parse(data.data);
+		if (notificationData.notification_type == global.CHAT_MESSAGE_NOTIFICATION) {
+			if (!this.props.IS_IN_CHAT)
+				this.props.setStore(global.U_MESSAGE_INCREMENT, 1);
+		}
+		else if (notificationData.notification_type == global.ACCOUNT_STATUS_NOTIFICATION) {
+			if (notificationData?.active == 0) {
+				// await SetPrefrence(global.PREF_REMEMBER_ME, 0);
+			}
+		}
+	} catch (error) {
+		console.log('remote notification received error', error);
+	}
 
-  return Promise.resolve(notification);
+	return Promise.resolve(notification);
 }
