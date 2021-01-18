f477a16b4ca766bd8239f80601eddd27bdc56bae
worker722 <tech.worker722@gmail.com>
2021-01-18 19:16:45 +0100

    dsdsds

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,9 @@
 import { Images } from "./images";
 import { BaseColor } from "./color";
-import { BaseStyle } from "./theme";
 import Responsive from "./responsive";
 
 export {
   Images,
   BaseColor,
-  BaseStyle,
   Responsive
 };
